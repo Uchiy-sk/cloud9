c701f24bc13d9ae3494df701f2dacd7e15918e48
EC2 Default User <ec2-user@ip-172-31-33-119.ec2.internal>
2022-10-05 05:25:15 +0000

    [finish] 3-2

--- sample_app/app/controllers/lists_controller.rb
+++ sample_app/app/controllers/lists_controller.rb
@@ -1,5 +1,12 @@
 class ListsController < ApplicationController
   def new
+    @list = List.new
+  end
+
+  def create
+    list = List.new(list_params)  # データを受け取り、インスタンスの作成
+    list.save                     # データをdbに保存
+    redirect_to '/top'            # top画面にリダイレクト
   end
 
   def index
@@ -9,5 +16,10 @@ class ListsController < ApplicationController
   end
 
   def edit
+  end
+
+  private # アクションとURLを非対応にして制限
+  def list_params
+    params.require(:list).permit(:title, :body)
   end
 end
